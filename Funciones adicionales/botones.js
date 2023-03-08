f1ffc36393da191d1187eb47fe33968634fe5c48
Ezequiel <94020478+Eze-92@users.noreply.github.com>
2023-03-08 16:27:43 -0300

    Initial commit

---
+++ Funciones adicionales/botones.js
@@ -0,0 +1,8 @@
+
+document.getElementById("boton_color").addEventListener("click", function() {
+    document.body.style.backgroundColor= "#FFF0F5";
+    })
+
+document.getElementById("boton_default").addEventListener("click", function() {
+    document.body.style.backgroundColor= "rgb(222, 233, 250)";
+})
